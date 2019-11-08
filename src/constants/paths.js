9013e0dab052299e8978f2b566bb7491368bea86
fuleinist <fuleinist@gmail.com>
2019-11-09 07:58:27 +1100

    nov

---
+++ src/constants/paths.js
@@ -0,0 +1,5 @@
+export const CAR_PATH = '/cars'
+export const LIST_PATH = '/projects'
+export const ACCOUNT_PATH = '/account'
+export const LOGIN_PATH = '/login'
+export const SIGNUP_PATH = '/signup'
